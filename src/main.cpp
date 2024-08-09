fdc17bc7931df3e722173a542b65efe308fdc189
Justin <52604018+hiimjustin000@users.noreply.github.com>
2024-08-09 09:50:55 -0400

    easy

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,10 @@
 #include <Geode/modify/GauntletSelectLayer.hpp>
 using namespace geode::prelude;
 class $modify(GauntletSelectLayer) {
-    bool init(int gt) {
-        return GauntletSelectLayer::init(gt);
-    }
-
     void setupGauntlets() {
         GauntletSelectLayer::setupGauntlets();
 
-        auto list = this->getChildByIDRecursive("gauntlets-list");
-        if (!list) return;
-
-        auto pages = typeinfo_cast<CCArray*>(list->getChildByID("gauntlet-pages")->getChildren());
-        if (pages->count() == 0) return;
-
-        auto page = typeinfo_cast<CCMenu*>(typeinfo_cast<CCLayer*>(pages->objectAtIndex(0))->getChildren()->objectAtIndex(0));
-        if (pages->count() == 1) {
-            page->setPosition({-285.f, page->getPosition().y});
-        }
+        if (m_scrollLayer->m_pages->count() == 0) return;
+        else if (m_scrollLayer->m_pages->count() == 1) static_cast<CCNode*>(m_scrollLayer->m_pages->objectAtIndex(0))->getChildByID("gauntlet-menu")->setPositionX(-285.f);
     }
 };
